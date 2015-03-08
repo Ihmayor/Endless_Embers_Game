8dcfae9bb92613fe73e5908f3dbd01d098dd4cbf
Matteo Molnar <matteo.molnar@gmail.com>
2015-03-08 17:56:10 -0600

    Manual Compilation into Master
    Manually merged my code and the current code in master.

---
+++ CURRENT MERGE/src/gameStates/Game.java
@@ -0,0 +1,127 @@
+package gameStates;
+
+import mapRelated.BasicMap;
+import monsterRelated.BasicMonster;
+import monsterRelated.Creature;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+import combatRelated.CombatManager;
+
+import playerRelated.Player;
+
+import java.util.LinkedList;
+
+public class Game extends BasicGameState {
+
+	private StateBasedGame game;
+	private BasicMap currentMap;
+	private BasicMap nextMap;
+	//Can maybe have an array of levels (maps) in the future
+	int floorCounter = 1;
+
+	private Player player;
+	private String[][] entityArray;
+	private BasicMonster monster;
+	
+	public static String statusUpdate;
+	private LinkedList <BasicMonster> monsterList = new LinkedList<BasicMonster>();
+	
+	private long prevTime;
+	
+	public static final int ID = 1;
+	@Override
+	public void init(GameContainer gc, StateBasedGame stateGame) throws SlickException {
+		
+		this.game = stateGame;
+		prevTime = 0;
+		currentMap = new BasicMap("res/map/singleTilePassageWay.tmx");
+		nextMap = new BasicMap("res/map/floor2.tmx");
+		
+		SpriteSheet monsterSheet = new SpriteSheet("res/player/dummySheet.png",32,32); 
+		Image monsterImage = monsterSheet.getSubImage(0, 0);
+		
+		//Set up the creatures that appear initially
+		player = new Player(gc, stateGame,currentMap, 4*32, 5*32);
+		monster = new BasicMonster(currentMap, monsterImage, 7*32, 11*32);
+		initEntityArray();
+		monsterList.add(monster);
+		CombatManager.setMonsterList(monsterList);
+		player.setEntityArray(entityArray);
+		monster.setEntityArray(entityArray);
+		
+		statusUpdate = "Game is Now In Session";
+	}
+
+	private void initEntityArray (){
+		String [][] newArray = new String [BasicMap.widthByTiles][BasicMap.heightByTiles];
+		entityArray = newArray;
+		for (int i = 0; i < BasicMap.widthByTiles; i++)
+		{
+			for (int c = 0; c < BasicMap.heightByTiles; c++)
+			{
+				entityArray[i][c] = " ";
+			}
+		}
+		entityArray[((Creature)player).getPosition()[0]/32][((Creature)player).getPosition()[1]/32] = player.getName();
+		entityArray[monster.getPosition()[0]/32][monster.getPosition()[1]/32] = monster.getName();
+	}
+	
+	@Override
+	public void render(GameContainer gc, StateBasedGame stateGame, Graphics g)
+			throws SlickException {
+		currentMap.render();
+		monster.render(g);
+		player.render(g);
+		g.setColor(Color.white);
+	    g.drawString(statusUpdate, 100, 490);
+	    g.drawString("Press Q to quit", 700, 490);
+	}
+	
+	@Override
+	public void keyReleased (int key,char c){
+	switch (key){
+	case Input.KEY_Q:
+		GameContainer gc = game.getContainer();//Had to instantiate. I could've also made this another class variable.
+		gc.exit();//Exits game. 	
+		break;
+		}	
+	}
+
+	int counter = 0;
+	@Override
+	public void update(GameContainer gc, StateBasedGame stateGame, int delta)
+			throws SlickException {
+		long tmp = System.currentTimeMillis();
+		prevTime = tmp;
+		player.update(counter);
+		
+		if (player.getOnStairs()){
+			player.setMap(nextMap);
+			currentMap = nextMap;
+			floorCounter++;
+			player.setOnStairs(false);
+		}
+		
+		//Prevent Monster from fleeing
+		if (CombatManager.battleHappening == false){
+			monster.update(player.getPosition(), counter++);
+			if (counter > 500)//Used to delay the monster's movement
+				counter = 0;
+			}
+	}
+	
+	@Override
+	public int getID() {
+		return ID;
+	}
+
+}
---
+++ CURRENT MERGE/src/playerRelated/PlayerTest.java
@@ -0,0 +1,21 @@
+package playerRelated;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Test
+	public void test() {
+		///Test Moving
+		///Testing Updating
+		///Test Render????
+		
+		
+		
+		
+		fail("Not yet implemented");
+	}
+
+}
