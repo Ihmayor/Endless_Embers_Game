db6e36527213c130d625ece3f925df82fe3efc10
Irene Mayor <ihmayor@ucalgary.ca>
2017-10-12 12:19:28 -0600

    Changed Ver2GameDriver screenwidth references to call GameWindowSettings class

--- Version 1.1/NewestVersion/src/gameStates/Ver2GameDriver.java
+++ Version 1.1/NewestVersion/src/gameStates/Ver2GameDriver.java
@@ -14,8 +14,8 @@ public class Ver2GameDriver {
 		
 		//All the code below is how we get this game rolling and running.
 		
-		final int screenWidth = 1120; // Set screen width to 35 tiles long (35*32px)
-		final int screenHeight = 512; // Set screen height to 16 tile long (16*32px)
+		final int screenWidth = GameWindowSettings.getScreenWidth(); // Set screen width to 35 tiles long (35*32px)
+		final int screenHeight = GameWindowSettings.getScreenHeight(); // Set screen height to 16 tile long (16*32px)
 		
 			try
 			{
