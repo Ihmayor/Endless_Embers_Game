dc6666c2c666a16e51d3d852f9c751aca4c7200b
Irene Mayor <ihmayor@ucalgary.ca>
2017-10-12 12:16:13 -0600

    Fixing screenwidth/screenheight shotgun surgery problem. Created GameWindow Class

---
+++ Version 1.1/NewestVersion/src/gameStates/GameWindowSettings.java
@@ -0,0 +1,12 @@
+package gameStates;
+
+public class GameWindowSettings {
+
+	private final int screenWidth = 1120;
+	private final int screenHeight = 800;
+	
+	public int getScreenWidth() {return screenWidth;}
+	public int getScreenHeight() {return screenHeight;}
+	
+	
+}
