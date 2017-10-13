9215db4dfaff54a8728ddcfb36510c7b1a6a3187
Irene Mayor <ihmayor@ucalgary.ca>
2017-10-12 21:08:42 -0600

    Refactored GameWindow To derive its information from basicMap

--- Version 1.1/NewestVersion/src/gameStates/GameWindowSettings.java
+++ Version 1.1/NewestVersion/src/gameStates/GameWindowSettings.java
@@ -1,11 +1,15 @@
 package gameStates;
 
+import mapRelated.BasicMap;
+
 public class GameWindowSettings {
 
-	private static final int screenWidth =1120;
-	private static final int screenHeight = 512;
+	private static final int screenWidth = BasicMap.TILESIZE*BasicMap.widthByTiles;
+	private static final int screenHeight = BasicMap.TILESIZE*BasicMap.heightByTiles;
 	
 	public static int getScreenWidth() {return screenWidth;}
 	public static int getScreenHeight() {return screenHeight;}
+	
+	
 	
 }
